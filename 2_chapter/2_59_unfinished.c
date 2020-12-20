e68e937ccdec978ab84b5f36ea4ae65d2816bb54
Alexey Ershov <alex4rom@gmail.com>
2020-12-20 22:22:04 +0300

    Update 2_59_unfinished.c

--- 2_chapter/2_59_unfinished.c
+++ 2_chapter/2_59_unfinished.c
@@ -1,1 +1,3 @@
-
+// Write a C expression that will yield a word consisting of the least significant byte of
+// x and the remaining bytes of y. For operands x=0x89ABCDEF and y=0x76543210,
+// this would give 0x765432EF
