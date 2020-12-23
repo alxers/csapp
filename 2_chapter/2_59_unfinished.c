c667501535697ed24162c242ae99f951c659cc8f
Alexey Ershov <alex4rom@gmail.com>
2020-12-23 22:34:24 +0300

    Update 2_59_unfinished.c

--- 2_chapter/2_59_unfinished.c
+++ 2_chapter/2_59_unfinished.c
@@ -1,3 +1,13 @@
 // Write a C expression that will yield a word consisting of the least significant byte of
 // x and the remaining bytes of y. For operands x=0x89ABCDEF and y=0x76543210,
 // this would give 0x765432EF
+
+#include <stdio.h>
+
+
+int main() {
+    int x = 0x89ABCDEF;
+    int y = 0x76543210;
+    printf("%x", x);
+    return 0;
+}
